2c5ff6f62b07b1695ee0a5b81b01e5c7741c605e
Biruk haile <birukhailegebeyehu11@gmail.com>
2026-04-25 15:17:12 +0300

    Added get method to retrieve all records (/medical)

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const port = 3000;
-const filepath = path.path(__dirname, "medical_record.json");
+const filePath = path.join(__dirname, "medical_record.json");
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
+  const { method, url } = req;
+
+  if (url === "/medical" && method === "GET") {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end(JSON.stringify({ error: "Could not read data" }));
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
 });
 
 server.listen(port, function (error) {
