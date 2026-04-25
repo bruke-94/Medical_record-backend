0d05b1d18409061438c625a6ec3f297ba89bc931
Biruk haile <birukhailegebeyehu11@gmail.com>
2026-04-25 20:48:20 +0300

    post method implemented with auto genereated id using date.now() method

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
   const { method, url } = req;
 
-  if (url === "/medical" && method === "GET") {
+  if (url === "/medical/" && method === "GET") {
     fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         res.writeHead(500);
@@ -15,6 +15,27 @@ const server = http.createServer((req, res) => {
       }
       res.writeHead(200);
       res.end(data);
+    });
+  } else if (url === "/medical" && method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const newRecord = JSON.parse(body);
+
+      fs.readFile(filePath, "utf8", (err, data) => {
+        const record = JSON.parse(data || "[]");
+        newRecord.id = Date.now().toString();
+        record.push(newRecord);
+
+        fs.writeFile(filePath, JSON.stringify(record, null, 2), (err) => {
+          res.writeHead(201);
+          res.end(JSON.stringify(newRecord));
+        });
+      });
     });
   }
 });
